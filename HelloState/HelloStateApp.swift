c0c7c6f071329cb8976b7d47a716ffa3dcd0d188
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-29 21:54:17 +0200

    init

---
+++ HelloState/HelloStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStateApp.swift
+//  HelloState
+//
+//  Created by Baptiste MARCON on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
